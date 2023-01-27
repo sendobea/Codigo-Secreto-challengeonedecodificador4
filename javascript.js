596fba0a94d82206c35e99aee9c6e0cbb0b06d76
Paula Beatriz <40565699+sendobea@users.noreply.github.com>
2023-01-27 20:56:34 -0300

    codigo JS ainda não está funcionando

--- javascript.js
+++ javascript.js
@@ -1,1 +1,53 @@
 <meta charset="utf-8">
+const encriptador = document.querySelector(".encriptador");
+const mensagem = document.querySelector(".mensagem");
+
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "e" é convertida para "enter"`
+// `A letra "i" é convertida para "imes"`
+// `A letra "a" é convertida para "ai"`
+// `A letra "o" é convertida para "ober"`
+// `A letra "u" é convertida para "ufat"`
+
+function botaoencriptar() {
+    const textoEncriptado = encriptar(encriptador.value);
+    mensagem.value = textoEncriptado;
+    encriptador.value = "";
+}
+
+
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+function botaodesencriptar() {
+    const textoDesencriptado = desencriptar(encriptador.value);
+    mensagem.value = textoDesencriptado;
+    encriptador.value = "";
+}
+
+
+function desencriptar()(stringDesencriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
